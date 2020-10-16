34cfde93dddc5d95b8bdca98e457be53782e1b21
Luan Alves <luan.alves@comunike.me>
2020-10-16 18:26:57 -0300

    fix: RegisterDonation()

--- BackEnd/src/controller/RegistroDoacaoController.js
+++ BackEnd/src/controller/RegistroDoacaoController.js
@@ -2,33 +2,31 @@ const User = require("../models/User");
 const RegistroDoacao = require("../models/RegistroDoacao");
 const moment = require("moment");
 
+
+
 module.exports = {
   async store(req, res) {
     var { dataDoacao } = req.body;
     const { localDoacao } = req.body;
     const { user_id } = req.params;
 
-    dataDoacao = new Date(moment(dataDoacao).format("YYYY-MM-DD"))
-    console.log(dataDoacao)
+    dataDoacao = new Date(moment(dataDoacao).add(1,"d").format("YYYY-MM-DD"))
     //Masculino 3 meses
     //Feminino  4 meses
     const user = await User.findById(user_id);
     var oldDonation;
     if (user) {
-      var lastRegister = await RegistroDoacao.find({user: user_id}).sort({
-        dateDoacao: -1,
-      });
-      if (lastRegister) {
-        oldDonation = lastRegister[lastRegister.length - 1].proximaDataDoacao 
-        console.log(oldDonation)
-        if (dataDoacao >= oldDonation) {
-          this.registerGenero()
+      const lastRegister = await (await RegistroDoacao.find({'user': user_id}).sort({'dataDoacao': -1}));
+      if (lastRegister.length !== 0) {
+        oldDonation = lastRegister[lastRegister.length - 1].proximaDataDoacao
+        if (dataDoacao > oldDonation) {
+          await registerGenero(dataDoacao,localDoacao,user)
           return res.status(200).json("Foi");
         } else {
           return res.status(200).json("Tera que esperar ze gotinha");
           }
       }else{
-        this.registerGenero()
+        await registerGenero(dataDoacao,localDoacao,user)
         return res.status(200).json("Primeiro Registro");
       }
     } else {
@@ -38,27 +36,26 @@ module.exports = {
 
   async alter(req, res) {},
   async deleted(req, res) {},
+};
 
+async function registerGenero(dataDoacao,localDoacao,user){
 
-  async registerGenero(){
+  var proximaDataDoacao = new Date(moment(dataDoacao).subtract(1,"day"))
     if (user.genero == "Masculino"){
           const registroDoacao = await RegistroDoacao.create({
-            dataDoacao: moment(dataDoacao).format("DD-MM-YYYY"),
+            dataDoacao: moment(dataDoacao).format("YYYY-MM-DD"),
             localDoacao,
-            user: user_id,
-            proximaDataDoacao: new Date().setMonth(new Date().getMonth + 3)
+            user: user._id,
+            proximaDataDoacao: new Date(proximaDataDoacao).setMonth(new Date(proximaDataDoacao).getMonth() + 3)
           });
           registroDoacao.save();
-          return res.status(200).json("Primeiro Registro");
         }else if (user.genero == "Feminino"){
             const registroDoacao = await RegistroDoacao.create({
-            dataDoacao: moment(dataDoacao).format("DD-MM-YYYY"),
+            dataDoacao: moment(dataDoacao).format("YYYY-MM-DD"),
             localDoacao,
-            user: user_id,
-            proximaDataDoacao: new Date().setMonth(new Date().getMonth + 4)
+            user: user._id,
+            proximaDataDoacao: new Date(dataDoacao).setMonth(new Date().getMonth() + 4)
           });
           registroDoacao.save()
         }
   }
-
-};
