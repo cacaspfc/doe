c57ecb67b0ab1e429f48dca77ec2645e6ae35575
Bugvoid <48994637+Bugvoid@users.noreply.github.com>
2020-10-26 22:15:44 -0300

    feat: One Page integrated

---
+++ BackEnd/src/controller/TrofeusController.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+const RegistroDoacao = require("../models/RegistroDoacao");
+
+module.exports = {
+  async show(){
+
+  }, 
+  async store(){
+
+  },
+
+}
+
