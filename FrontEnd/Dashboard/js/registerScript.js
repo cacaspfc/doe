e9a17544abbf441f9e6cd60807b7b1c3041baf61
Bugvoid <48994637+Bugvoid@users.noreply.github.com>
2020-11-03 21:22:10 -0300

    Feat: Return Dados Doacao

--- FrontEnd/Dashboard/js/registerScript.js
+++ FrontEnd/Dashboard/js/registerScript.js
@@ -4,7 +4,7 @@ function registrarDoacao(event) {
   event.preventDefault();
   var genero = document.getElementById('sexo').value;
   if (genero == '') {
-    alert("preencha seus dados cadastrais primeiro")
+    alert('preencha seus dados cadastrais primeiro');
   } else {
     var dataDoacao = document.getElementById('dataDoacao').value;
     var localDoacao = document.getElementById('localDoacao').value;
@@ -27,3 +27,35 @@ function registrarDoacao(event) {
     });
   }
 }
+$(document).ready(function () {
+  fillTable();
+});
+//fetch api (AJAX) to fill table
+fillTable = () => {
+  var user_id = localStorage.getItem('User');
+  fetch(baseUrl + '/userdoacao/' + user_id)
+    .then((response) => response.json())
+    .then((data) => {
+      let html = '';
+      for (i = 0; i < data.length; i++) {
+        html +=
+          '<tr>' +
+          '<td class="">' +
+          data[i].dataDoacao +
+          '</td>' +
+          '<br/><br/>' +
+          '<td class="">' +
+          data[i].localDoacao +
+          '</td>' +
+          '<br/><br/>' +
+          '<td class="pv3 w-30 pr3 bb b--black-20">' +
+          '<div class="btn-group" role="group" aria-label="Basic example">' +
+          '</div>' +
+          '</td>' +
+          '</tr>';
+      }
+      $('#tblBody').html(html);
+    })
+    .catch((err) => console.log('ERROR!: ', err));
+};
+function showRegister(event) {}
