161603c7da494d053c0d10aa2d43d658601a2946
Bugvoid <48994637+Bugvoid@users.noreply.github.com>
2020-11-14 21:44:23 -0300

    fix: URL Heroku

--- FrontEnd/Dashboard/js/trophyScript.js
+++ FrontEnd/Dashboard/js/trophyScript.js
@@ -1,4 +1,4 @@
-var baseUrl = 'http://localhost:8000';
+var baseUrl = 'https://doeparceiro.herokuapp.com';
 
 window.onload = showTrophie(new Event('event'));
 
@@ -16,58 +16,57 @@ function showTrophie(event) {
           }
         }
 
-          if (data.trofeus[i] == 'T1') {
-            var lista = document.querySelectorAll('#T1').length;
-            for (let i = 0; i < lista; i++) {
-              document.querySelectorAll('#T1')[i].style.filter = 'grayscale(0%)';
-            }
+        if (data.trofeus[i] == 'T1') {
+          var lista = document.querySelectorAll('#T1').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#T1')[i].style.filter = 'grayscale(0%)';
           }
+        }
 
-          if (data.trofeus[i] == 'T2') {
-            var lista = document.querySelectorAll('#T2').length;
-            for (let i = 0; i < lista; i++) {
-              document.querySelectorAll('#T2')[i].style.filter = 'grayscale(0%)';
-            }
+        if (data.trofeus[i] == 'T2') {
+          var lista = document.querySelectorAll('#T2').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#T2')[i].style.filter = 'grayscale(0%)';
           }
+        }
 
-            if (data.trofeus[i] == 'T3') {
-              var lista = document.querySelectorAll('#T3').length;
-              for (let i = 0; i < lista; i++) {
-                document.querySelectorAll('#T3')[i].style.filter = 'grayscale(0%)';
-              }
-            }
-            if (data.trofeus[i] == 'T4') {
-              var lista = document.querySelectorAll('#T4').length;
-              for (let i = 0; i < lista; i++) {
-                document.querySelectorAll('#T4')[i].style.filter = 'grayscale(0%)';
-              }
+        if (data.trofeus[i] == 'T3') {
+          var lista = document.querySelectorAll('#T3').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#T3')[i].style.filter = 'grayscale(0%)';
           }
-            if (data.trofeus[i] == 'SF1') {
-              var lista = document.querySelectorAll('#SF1').length;
-              for (let i = 0; i < lista; i++) {
-                document.querySelectorAll('#SF1')[i].style.filter = 'grayscale(0%)';
-              }
+        }
+        if (data.trofeus[i] == 'T4') {
+          var lista = document.querySelectorAll('#T4').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#T4')[i].style.filter = 'grayscale(0%)';
           }
-          if (data.trofeus[i] == 'SF2') {
-            var lista = document.querySelectorAll('#SF2').length;
-            for (let i = 0; i < lista; i++) {
-                document.querySelectorAll('#SF2')[i].style.filter = 'grayscale(0%)';
-              }
-            }
-            if (data.trofeus[i] == 'SF3') {
-              var lista = document.querySelectorAll('#SF3').length;
-              for (let i = 0; i < lista; i++) {
-                document.querySelectorAll('#SF3')[i].style.filter = 'grayscale(0%)';
-              }
-            }
-            if (data.trofeus[i] == 'SF4') {
-              var lista = document.querySelectorAll('#SF4').length;
-              for (let i = 0; i < lista; i++) {
-                document.querySelectorAll('#SF4')[i].style.filter = 'grayscale(0%)';
-              }
+        }
+        if (data.trofeus[i] == 'SF1') {
+          var lista = document.querySelectorAll('#SF1').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#SF1')[i].style.filter = 'grayscale(0%)';
           }
+        }
+        if (data.trofeus[i] == 'SF2') {
+          var lista = document.querySelectorAll('#SF2').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#SF2')[i].style.filter = 'grayscale(0%)';
           }
-
-        })
-        .catch((err) => console.log('ERROR!: ', err));
+        }
+        if (data.trofeus[i] == 'SF3') {
+          var lista = document.querySelectorAll('#SF3').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#SF3')[i].style.filter = 'grayscale(0%)';
+          }
+        }
+        if (data.trofeus[i] == 'SF4') {
+          var lista = document.querySelectorAll('#SF4').length;
+          for (let i = 0; i < lista; i++) {
+            document.querySelectorAll('#SF4')[i].style.filter = 'grayscale(0%)';
+          }
+        }
+      }
+    })
+    .catch((err) => console.log('ERROR!: ', err));
 }
