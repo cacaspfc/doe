1e0b0f01890d5df6303aa1714e3e6e91399ce97b
Bugvoid <48994637+Bugvoid@users.noreply.github.com>
2020-11-14 21:37:40 -0300

    feat: Tests

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -19,12 +19,7 @@ routes.get('/userdoacao/:user_id', RegistroDoacaoController.show);
 routes.get('/usertrophy/:user_id', TrofeusController.show);
 
 routes.get('/home', function (request, response) {
-  // if (request.session.loggedin) {
-  //   response.send("Welcome back, " + request.session.username + "!");
-  // } else {
-  //   response.send("Please login to view this page!");
-  // }
-  // response.end();
+  response.json('TESTE');
 });
 
 module.exports = routes;
